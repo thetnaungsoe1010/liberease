36ef27af63de69e3a08faf908b4b33f85186fd04
HtutKyaw99 <71307967+HtutKyaw99@users.noreply.github.com>
2023-08-19 13:19:57 +0630

    fist commit

---
+++ book_slick.js
@@ -0,0 +1,9 @@
+$(function () {
+  $(".slick-carousel").slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: true,
+    dots: true,
+  });
+});
---
+++ home_slick.js
@@ -0,0 +1,9 @@
+$(function () {
+  $(".slick-carousel").slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+  });
+});
